d9b7e19976bd93057ee40a2f48213d5ea4330380
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-02-06 23:28:54 +0530

    display more info on meal item

--- lib/widget/meal_item.dart
+++ lib/widget/meal_item.dart
@@ -10,6 +10,46 @@ class MealItem extends StatelessWidget {
   final Complexity complexity;
   final Affordability afordability;
 
+  String get complexityText{
+    switch(complexity)
+    {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      default:
+        return 'Unknown';
+        break;
+
+    }
+  }
+
+
+   String get affordText{
+    switch(afordability)
+    {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        return 'Luxurious';
+        break;
+      default:
+        return 'Unknown';
+        break;
+
+    }
+  }
+
+
 
   MealItem({
     @required this.title,
@@ -36,13 +76,57 @@ class MealItem extends StatelessWidget {
           children: <Widget>[
             Stack(
               children: <Widget>[
-                 ClipRRect(
+                ClipRRect(
                   borderRadius: BorderRadius.only(topLeft:Radius.circular(15),topRight: Radius.circular(15)),
                   child: Image.network(imageUrl,height : 250,width: double.infinity,fit:BoxFit.cover ,),
+                ),
+                Positioned(
+                  //positioned is used to provide position
+                  //bottom means left 20 size at bottom and then start to display
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 5,horizontal: 20),
+                    width: 300,
+                    color: Colors.black54,
+                    child: Text(
+                      title,
+                      style:TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      //auto wrap the text if length is big
+                      softWrap: true,
+
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 ),
               ],
 
             ),
+            Padding(
+              padding:const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                Row(children: <Widget>[
+                  Icon(Icons.schedule),
+                  SizedBox(width:6),
+                  Text('$duration min'),
+                ],),
+                Row(children: <Widget>[
+                  Icon(Icons.work),
+                  SizedBox(width:6),
+                  Text(complexityText),
+                ],),
+                Row(children: <Widget>[
+                  Icon(Icons.attach_money),
+                  SizedBox(width:6),
+                  Text(affordText),
+                ],),
+              ],),
+            )
           ],
         ),
       ),
