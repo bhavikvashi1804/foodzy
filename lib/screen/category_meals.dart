d3dc35bc42d8eb7f4abde597532009a8d4be6f40
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-02-06 22:29:40 +0530

    add more data to meals

--- lib/screen/category_meals.dart
+++ lib/screen/category_meals.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+
+import '../models/dummy_categories.dart';
+
 class CategoryMealsPage extends StatelessWidget {
 
 
@@ -20,13 +23,19 @@ class CategoryMealsPage extends StatelessWidget {
 
     final categoryTitle=routeArgs['title'];
     final categoryID=routeArgs['id'];
+    final categoryMeals=DUMMY_MEALS.where((element) {
+      return element.categories.contains(categoryID);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child:Text("The recipes for the category"),
+      body: ListView.builder(
+        itemBuilder: (ctx,index){
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
